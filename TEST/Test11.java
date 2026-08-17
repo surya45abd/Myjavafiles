5a69b42f9d51d925ebf4ed84f953288a1d42397b
surya45abd <gouthamravisgr@gmail.com>
2026-08-17 15:40:59 +0530

    updated17

---
+++ TEST/Test11.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Test11{
+    public static void main(String[] args){
+        int x = 23;
+        for(int i = 1; i<=x;i++){
+            System.out.println(i);
+        }
+        
+    }
+}
