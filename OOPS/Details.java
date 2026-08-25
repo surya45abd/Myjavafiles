eee24cd04f9ff10cc62d462684476e3ca41c9b34
surya45abd <gouthamravisgr@gmail.com>
2026-08-25 18:29:23 +0530

    25updated

---
+++ OOPS/Details.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Details{
+    public static void main(String[] args){
+        Student s = new Student();
+        s.num = 10;
+        s.name = "surya";
+        s.sal = 100000;
+        s.display();
+    }
+}
---
+++ OOPS/Student.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Student{
+    int num;
+    int sal;
+    String name;
+
+    void display(){
+        System.out.println(num);
+        System.out.println(name);
+        System.out.println(sal);
+    }
+
+}
